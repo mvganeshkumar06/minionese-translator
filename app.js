01401bc19848a2f56363e986d2689e237a767359
Ganesh Kumar <mvganeshk15sde@gmail.com>
2020-12-01 15:03:38 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const input = document.querySelector("#input textarea");
+const btn = document.querySelector("button");
+const output = document.querySelector("#output textarea");
+const alertModal = document.querySelector("#alertModal");
+
+btn.addEventListener("click", () => {
+  output.value = "";
+
+  if (!input.value) {
+    alertModal.style.display = "inline";
+    setTimeout(() => {
+      alertModal.style.display = "none";
+    }, 2000);
+  } else {
+    alertModal.style.display = "none";
+    fetch(
+      `https://api.funtranslations.com/translate/minion.json?text=${input.value}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        output.value = data.contents.translated;
+      })
+      .catch((error) => {
+        console.log(error, error.message);
+      });
+  }
+  input.value = "";
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  output.value = "";
+});
